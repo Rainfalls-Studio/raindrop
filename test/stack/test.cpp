439aa4c02af33caf26a56a582983fba90e5771fd
Aalex <aaleex3984@gmail.com>
2022-11-16 17:56:05 +0100

    stack

--- test/stack/test.cpp
+++ test/stack/test.cpp
@@ -8,16 +8,18 @@ int main(int argc, char** argv){
 
 	rnd::memory::Stack stack;
 	stack.init(10000);
+	RND_LOG("initialization", "success");
 
 	for (int i=0; i<500; i++){
 		stack.allocate(rand() % 25);
 	}
+	RND_LOG("allocation", "success");
 
 	stack.reset();
+	RND_LOG("reset", "success");
 
 	stack.shutdown();
-
-	RND_LOG("end", "end successfully");
+	RND_LOG("shutdown", "success");
 
 	PROFILE_END_SESSION();
 	RND_LOG_SHUTDOWN();
