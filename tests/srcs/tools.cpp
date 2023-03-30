9dea269194c03f31581ce801278a43e4d3c30cd7
Aalex <aaleex3984@gmail.com>
2023-03-30 21:30:28 +0200

    update file organization

--- tests/srcs/tools.cpp
+++
@@ -1,8 +0,0 @@
-#include "test.hpp"
-
-TEST(bitwise, shiftOffset){
-	for (Raindrop::uint32 i=0; i<64; i++){
-		Raindrop::uint64 value = 1ULL << i;
-		EXPECT_EQ(i, Raindrop::Tools::getShiftOffset(value));
-	}
-}
