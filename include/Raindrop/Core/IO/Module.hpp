b9b8344981c8218cd49fcdc561e297f28736f99c
Aalex <aaleex3984@gmail.com>
2023-05-11 13:45:21 +0200

    load plugin from module

--- include/Raindrop/Core/IO/Module.hpp
+++ include/Raindrop/Core/IO/Module.hpp
@@ -1,7 +1,7 @@
 #ifndef __RAINDROP_CORE_IO_MODULE_HPP__
 #define __RAINDROP_CORE_IO_MODULE_HPP__
 
-#include <Core/IO/DLLReader.hpp>
+#include <Core/IO/common.hpp>
 
 namespace Raindrop::Core::IO{
 	class RAINDROP_API Module{
@@ -12,6 +12,22 @@ namespace Raindrop::Core::IO{
 			bool isOpen() const;
 			const std::filesystem::path& filepath() const;
 			void* getFnc(const char* name) const;
+
+			template<typename T, typename... Args>
+			std::shared_ptr<T> loadPlugin(const char* name, Args&&... args){
+				using FncType = std::shared_ptr<T>(*)(Args...);
+				std::string fncName = "create_" + std::string(name);
+
+				FncType fnc = nullptr;
+				
+				try{
+					fnc = (FncType)_dll.getProc(fncName.c_str());
+				} catch (const std::exception &e){
+					throw e;
+				}
+
+				return fnc(args...);
+			}
 		
 		private:
 			DLLReader _dll;
